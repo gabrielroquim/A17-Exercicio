3d87fb3aa23403197c3ca5673a4a688e79227707
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-04-15 10:52:14 -0300

    wdio configurado

---
+++ wdio.conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+    hostname: 'localhost',
+    port: 4723,
+    path: '/wd/hub',
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    framework: 'mocha',
+    capabilities: [{
+        "platformName": "Android",
+        "platformVersion": "12.0",
+        "deviceName": "TesteEbac",
+        "automationName": "UiAutomator2",
+        "app": join(process.cwd(), '.\\app\\android\\loja-ebac.apk'),
+        "appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity',
+        'newCommandTimeout': 240
+    }]
