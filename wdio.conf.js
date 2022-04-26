86a7b78eeaa5e7c6e08bd2b54149428c9fff8674
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-04-26 18:27:34 -0300

    instalando allure

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,5 @@
-const {join} =  require('path')
-
+const { join } = require('path')
+const allure = require('allure-commandline')
 exports.config = {
     hostname: 'localhost',
     port: 4723,
@@ -15,11 +15,41 @@ exports.config = {
         "automationName": "UiAutomator2",
         "app": join(process.cwd(), './app/android/loja-ebac.apk'),
         "appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity'
-       
+
     }],
     waitForTimeout: 20000,
-    mochaOpts:{
-        timeout:300000
+    mochaOpts: {
+        timeout: 300000
     },
-    reporters:['spec']
+    reporters: ['spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false,
+        }],
+    ],
+    onComplete: function () {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function (exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
+    afterStep: function (test, scenario, { error, duration, passed }) {
+        driver.takeScreenshot();
+
+    }
 }
