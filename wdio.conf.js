83be5f01a59908ea82e9e5f9702631d3947b4551
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-04-26 20:26:07 -0300

    salve\!

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,21 +3,31 @@ const allure = require('allure-commandline')
 const video = require('wdio-video-reporter')
 
 exports.config = {
-   // hostname: 'localhost',
-    port: 4723,
-   // path: '/wd/hub',
-   services: ['appium'],
+    // hostname: 'localhost',
+   // port: 4723,
+    // path: '/wd/hub',
+    user: "gabrielroquim_j2H5F4",
+    key: "ZV6tSgzyw2ytsxvpUyRF",
+    // services: ['appium'],
+    services: ['browsertack'],
     specs: [
         './test/specs/**/*.spec.js'
     ],
     framework: 'mocha',
     capabilities: [{
         "platformName": "Android",
-        "platformVersion": "12.0",
-        "deviceName": "TesteEbac",
-        "automationName": "UiAutomator2",
-        "app": join(process.cwd(), './app/android/loja-ebac.apk'),
-        "appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity'
+        // "platformVersion": "12.0",
+        // "deviceName": "TesteEbac",
+        // "automationName": "UiAutomator2",
+        //  "app": join(process.cwd(), './app/android/loja-ebac.apk'),
+        //"appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity'
+
+        'app': 'bs://007e11c759fd16b0a2085e604ef65d16ff58ece4',
+        'device': 'Samsung Galaxy Note 20',
+        'os_version': '10.0',
+        'project': 'Meu primeiro Device Farm',
+        'build': 'browserstack-build-1',
+        'name': 'teste_ebac'
 
     }],
     waitForTimeout: 20000,
@@ -31,8 +41,8 @@ exports.config = {
             disableWebdriverScreenshotsReporting: true,
         }],
         [video, {
-            saveAllVideos: true,      
-            videoSlowdownMultiplier: 50, 
+            saveAllVideos: true,
+            videoSlowdownMultiplier: 50,
         }]
     ],
     onComplete: function () {
