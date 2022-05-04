106843f2f1f20c2bc858d5470820ebab7455a4f6
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-05-04 20:29:50 -0300

    corrigindo a descricao

--- test/screens/adicionaProdutos.screen.js
+++ test/screens/adicionaProdutos.screen.js
@@ -1,7 +1,7 @@
-class AdicProdutosScreen {   
+class AdicProdutosScreen {
     get #simplePhyProduct() { return $('android=new UiSelector().index(0).className("android.view.ViewGroup")') }
     get #nomeProduto() { return $('id=editText') }
-    get #addDescricaoProduto() { return $('///android.widget.ImageView[@content-desc="Edit product"])[1]') }
+    get #addDescricaoProduto() { return $('//android.widget.ImageView[@content-desc="Edit product"])[1]') }
     get #descriptionProduto() { return $('id=visualEditor') }
     get #voltarMenu() { return $('~Navigate up') }
     get #addPriceProduct() { return $('android=new UiSelector().text("Add price").className("android.widget.TextView")') }
@@ -10,14 +10,19 @@ class AdicProdutosScreen {
     get #typePriceProduct() { return $('android=new UiSelector().text("0").className("android.widget.EditText")') }
     get #btnPublish() { return $('id=menu_publish') }
 
-   
+
     async clickSimplePhyProduct() {
         await this.#simplePhyProduct.waitForExist({ timeout: 20000 })
         return await this.#simplePhyProduct.click()
     }
     async typeName(nome) { return await this.#nomeProduto.setValue(nome) }
-    async clickDescribeYourProduct() { return await this.#addDescricaoProduto.click() }
-    async typeDescription(descricao) { return await this.#descriptionProduto.setValue(descricao) }
+    async clickDescribeYourProduct() {
+        await this.#addDescricaoProduto.waitForExist({ timeout: 20000 })
+        return await this.#addDescricaoProduto.click()
+    }
+    async typeDescription(descricao) { 
+        await this.#descriptionProduto.waitForExist({ timeout: 20000 })
+        return await this.#descriptionProduto.setValue(descricao) }
     async getVoltar() { return await this.#voltarMenu.click() }
     async goPrice() { return await this.#addPriceProduct.click() }
     async clickPriceProduto() { return await this.#priceProduto.click() }
